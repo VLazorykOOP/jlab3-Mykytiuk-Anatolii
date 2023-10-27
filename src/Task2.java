f4dbb2732e50c12dbd219b5c2af3f8b368341ffe
Mykytiuk-Anatolii <mykytiuk.anatolii@chnu.edu.ua>
2023-10-27 11:21:25 +0300

    task2_final

--- src/Task2.java
+++ src/Task2.java
@@ -93,29 +93,44 @@ public class Task2 {
         Pair add(Pair other) {
             if (other instanceof Fraction) {
                 Fraction otherFraction = (Fraction) other;
-                // Реалізація додавання двох дробів
                 int commonDenominator = this.denominator * otherFraction.denominator;
                 int sumNumerator = this.numerator * otherFraction.denominator +
                         otherFraction.numerator * this.denominator;
-                return new Fraction(sumNumerator, commonDenominator);
+                return simplifyFraction(sumNumerator, commonDenominator);
             } else {
                 throw new IllegalArgumentException("Invalid operation: cannot add Fraction and non-Fraction.");
             }
         }
-
+        
         @Override
         Pair subtract(Pair other) {
             if (other instanceof Fraction) {
                 Fraction otherFraction = (Fraction) other;
-                // Реалізація віднімання двох дробів
                 int commonDenominator = this.denominator * otherFraction.denominator;
                 int diffNumerator = this.numerator * otherFraction.denominator -
                         otherFraction.numerator * this.denominator;
-                return new Fraction(diffNumerator, commonDenominator);
+                return simplifyFraction(diffNumerator, commonDenominator);
             } else {
                 throw new IllegalArgumentException("Invalid operation: cannot subtract Fraction and non-Fraction.");
             }
         }
+        
+        private Fraction simplifyFraction(int numerator, int denominator) {
+            int gcd = gcd(numerator, denominator);
+            numerator /= gcd;
+            denominator /= gcd;
+            return new Fraction(numerator, denominator);
+        }
+        
+        private int gcd(int a, int b) {
+            while (b != 0) {
+                int temp = b;
+                b = a % b;
+                a = temp;
+            }
+            return a;
+        }
+        
 
         @Override
         Pair multiply(int factor) {
@@ -147,27 +162,41 @@ public class Task2 {
         }
     }
 
+    
     public static void task2() {
-        Pair[] pairs = new Pair[3];
+        Pair[] pairs = new Pair[5];
         pairs[0] = new Money(50);
-        pairs[1] = new Fraction(1, 2);
-        pairs[2] = new Money(30);
-
+        pairs[1] = new Money(30);
+        pairs[2] = new Fraction(2, 3);
+        pairs[3] = new Fraction(1, 4);
+        pairs[4] = new Fraction(1, 2);
+    
         System.out.println("Pairs before operations:");
         for (Pair pair : pairs) {
             System.out.println(pair.toString());
         }
-
+    
         // Виконання арифметичних операцій та виведення результатів
         for (Pair pair : pairs) {
             System.out.println("Object: " + pair.toString());
-            System.out.println("Add 10: " + pair.add(new Money(10)).toString());
-            System.out.println("Subtract 5: " + pair.subtract(new Money(5)).toString());
-            System.out.println("Multiply by 2: " + pair.multiply(2).toString());
-            try {
-                System.out.println("Divide by 3: " + pair.divide(3).toString());
-            } catch (ArithmeticException e) {
-                System.out.println("Division by zero.");
+            if (pair instanceof Money) {
+                System.out.println("Add 10: " + ((Money) pair).add(new Money(10)).toString());
+                System.out.println("Subtract 5: " + ((Money) pair).subtract(new Money(5)).toString());
+                System.out.println("Multiply by 2: " + ((Money) pair).multiply(2).toString());
+                try {
+                    System.out.println("Divide by 3: " + ((Money) pair).divide(3).toString());
+                } catch (ArithmeticException e) {
+                    System.out.println("Division by zero.");
+                }
+            } else if (pair instanceof Fraction) {
+                System.out.println("Add 1/3: " + ((Fraction) pair).add(new Fraction(1, 3)).toString());
+                System.out.println("Subtract 1/6: " + ((Fraction) pair).subtract(new Fraction(1, 6)).toString());
+                System.out.println("Multiply by 2: " + ((Fraction) pair).multiply(2).toString());
+                try {
+                    System.out.println("Divide by 4: " + ((Fraction) pair).divide(4).toString());
+                } catch (ArithmeticException e) {
+                    System.out.println("Division by zero.");
+                }
             }
             System.out.println("-------------------------");
         }
